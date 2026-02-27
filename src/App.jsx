8f95bd8e05562585dd61dd9c054ae4a97d2fe136
Ifunanyadimma <ifunanyadimma20@gmail.com>
2026-02-27 23:59:51 +0100

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-700">
+        MedicPadi is Working 
+      </h1>
+    </>
+  )
+}
+
+export default App
